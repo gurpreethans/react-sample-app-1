0f8ab927b412c8d9c364bf2a45c4a8ff327dacb3
rordev <rordev@ongraph.com>
2019-03-27 19:00:28 +0530

    Inline style added

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -2,9 +2,19 @@ import React from 'react';
 import './UserInput.css';
 
 const UserInput = (props) => {
+
+  const inputStyle = {
+    border: '2px solid red'
+  };
+
+
 	return(
 		<div className="user-name">
-			<input type="text" onChange={props.change} value={props.username} />
+			<input 
+        type="text" 
+        style={inputStyle}
+        onChange={props.change} 
+        value={props.username} />
 		</div>
 	);
 }
